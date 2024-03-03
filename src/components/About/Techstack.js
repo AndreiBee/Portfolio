6d22e4459b3a5407fa4ec4f19c427cd3271bf737
andrei.pchalavodau <andrei.pchalavodau@rigaku.com>
2024-03-03 15:29:24 +0100

    Cleared some files

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -5,19 +5,17 @@ import {
   DiJavascript1,
   DiReact,
   DiNodejs,
-  DiMongodb,
   DiPython,
   DiGit,
-  DiJava,
 } from "react-icons/di";
-import {
-  SiRedis,
-  SiFirebase,
-  SiNextdotjs,
-  SiSolidity,
-  SiPostgresql,
-} from "react-icons/si";
-import { TbBrandGolang } from "react-icons/tb";
+// import {
+//   SiRedis,
+//   SiFirebase,
+//   SiNextdotjs,
+//   SiSolidity,
+//   SiPostgresql,
+// } from "react-icons/si";
+// import { TbBrandGolang } from "react-icons/tb";
 
 function Techstack() {
   return (
